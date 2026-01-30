382049fb27217f50564dad8cab594641c1ee7dd4
piguin <piguin.net@gmail.com>
2026-01-30 13:48:01 +0900

    STUNプロトコルの実装を追加

--- src/main/java/com/example/Utils.java
+++ src/main/java/com/example/Utils.java
@@ -4,4 +4,17 @@ public class Utils {
     public static int ushort2int(short value) {
         return (value < 0 ? 1 + Short.MAX_VALUE : 0) + (value & Short.MAX_VALUE);
     }
+
+    public static void printBytes(byte[] data) {
+        int i = 0;
+        while (i < data.length) {
+            int j = 0;
+            while (i < data.length && j < 4) {
+                System.out.print(String.format(" %02x", data[i]));
+                i++;
+                j++;
+            }
+            System.out.println();
+        }
+    }
 }
